3fcfea3e4770f14d0e789552a55e864b30f4c6fc
ghada <gh.abassi01@gmail.com>
2024-05-19 10:02:23 +0100

    addModules

---
+++ Ecommerce/domainService/src/main/java/com/micro/domainservice/dto/Order.java
@@ -0,0 +1,17 @@
+package com.micro.domainservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private String orderId;
+    private String name;
+    private int qt;
+
+    private double price;
+}
---
+++ Ecommerce/orderService/src/main/java/com/micro/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,14 @@
+package com.micro.orderservice.kafka;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private NewTopic topic ;
+
+
+}
