b21110b53fdc15ebcf5e55a49a548022c09fa7d7
ghada <gh.abassi01@gmail.com>
2024-07-09 14:00:36 +0100

    ProductDTO

--- Backend/productService/src/main/java/com/micro/productservice/entities/ProductDTO.java
+++ Backend/productService/src/main/java/com/micro/productservice/entities/ProductDTO.java
@@ -2,6 +2,8 @@ package com.micro.productservice.entities;
 
 
 import com.micro.productservice.enums.Category;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,12 +15,17 @@ import java.math.BigDecimal;
 public class ProductDTO {
 
     private int id;
+    @NotNull(message="Product name is required")
     private String name;
+    @NotNull(message="Product description is required")
     private String description;
     private String fileName;
     private String fileData;
+    @NotNull(message="Product category is required")
     private Category category;
+    @Positive(message="Quantity should be positive")
     private double qt;
+    @Positive(message="Price should be positive")
     private BigDecimal price;
 
 
