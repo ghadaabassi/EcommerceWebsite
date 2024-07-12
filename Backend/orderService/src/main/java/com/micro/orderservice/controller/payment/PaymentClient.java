b4642ccd68e254a1b4f65239c4697a653fbf2460
ghada <gh.abassi01@gmail.com>
2024-07-12 15:01:51 +0100

    smallFixFeign

--- Backend/orderService/src/main/java/com/micro/orderservice/controller/payment/PaymentClient.java
+++ Backend/orderService/src/main/java/com/micro/orderservice/controller/payment/PaymentClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "product-service",
+        name = "productService",
         url = "${application.config.payment-url}"
 )
 public interface PaymentClient {
