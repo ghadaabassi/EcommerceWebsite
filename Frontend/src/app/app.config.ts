dfbab3cf67207659372a167af2b26e1cc826a91d
ghada <gh.abassi01@gmail.com>
2024-09-09 17:02:05 +0100

    integrateKeycloakInAngularFrontEnd

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,13 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {KeycloakService} from './services/keycloak/keycloak.service';
+import {HttpTokenInterceptor} from './interceptor/http-token.interceptor';
 
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+
+} from '@angular/common/http';
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      deps: [KeycloakService],
+      useFactory: kcFactory,
+      multi: true
+    }
   ],
 };
--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductsService } from '../../services/products.service';
+import { ProductsService } from '../../services/product/products.service';
 @Component({
   selector: 'app-home',
   standalone: true,
